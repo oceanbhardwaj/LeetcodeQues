ab6ab2cfb69767de6d12a2de6ec2bc0480b2c311
oceanbhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2020-07-24 16:43:59 +0530

    Add files via upload

---
+++ WAterBottles.java
@@ -0,0 +1,29 @@
+class Solution {
+ 
+   public int numWaterBottles(int numBottles, int numExchange) {
+
+        int sum=numBottles;
+  
+      int blank=numBottles;
+    
+    int mod=0;
+     
+   int d=0;
+        
+while(blank>=numExchange)
+    
+    {
+            d=blank/numExchange;
+        
+    mod=blank%numExchange;
+         
+   sum+=d;
+        
+    blank=d+mod;
+      
+  }
+       
+ return sum;
+        
+    }
+}
