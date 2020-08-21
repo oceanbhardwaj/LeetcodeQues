4599331f5a408bd3515c61abaa5bffd63d90ea8c
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2020-08-21 20:03:07 +0530

    Add files via upload

---
+++ EditDistanceleetcode.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int n=word1.length();
+        int m=word2.length();
+        int dp[][]=new int[n+1][m+1];
+        for(int i=0;i<=n;i++)
+        {
+            for(int j=0;j<=m;j++)
+            {
+                if(i==0)
+                    dp[i][j]=j;
+                if(j==0)
+                    dp[i][j]=i;
+            }
+        }
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(word1.charAt(i-1)==word2.charAt(j-1))
+                    dp[i][j]=dp[i-1][j-1];
+                else
+                {
+                    int d=Math.min(dp[i-1][j],dp[i-1][j-1]);
+                    dp[i][j]=Math.min(dp[i][j-1],d)+1;
+                }
+            }
+        }
+        return dp[n][m];
+    }
+}
