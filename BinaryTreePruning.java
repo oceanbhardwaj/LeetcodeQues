dc641c3780b642e238179da2474f0f2f1e4c2683
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2020-12-02 20:01:14 +0530

    Add files via upload

--- BinaryTreePruning.java
+++ BinaryTreePruning.java
@@ -70,3 +70,51 @@ class Solution {
     }
     
 }
+//.............................................................................................................................................................................................
+class Solution {
+     
+    public TreeNode pruneTree(TreeNode root) {
+        if(root==null)
+            return null;
+           
+        return dfs(root)?root:null;
+    }
+    public boolean dfs(TreeNode root)
+    {
+        if(root==null)
+            return false;
+        boolean left=dfs(root.left);
+        boolean right=dfs(root.right);
+        if(!left)
+            root.left=null;
+        if(!right)
+            root.right=null;
+        return left || right || root.val==1;
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
+
